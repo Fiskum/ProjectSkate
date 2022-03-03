3b9e613ac77140827545e1fdb7304dab64e50528
Håvard Fiskum <haavard.f.123@live.no>
2022-03-03 17:15:46 +0100

    SliderTempoDONE

--- Assets/Script/TempoChanger.cs
+++ Assets/Script/TempoChanger.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempoChanger : MonoBehaviour
 {
     public AudioSource audioSource;
 
-    [Range(0.5f, 2)]
+    [Range(0.6f, 1.4f)]
     public float audioSpeed;
+    public Slider pitch;
 
     private void Start()
     {
+        
+        audioSpeed = 1;
         audioSource.pitch = audioSpeed;
-        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / audioSpeed);
+        //audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / audioSpeed);
     }
     void Update()
     {
         audioSource.pitch = audioSpeed;
-        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / audioSpeed);
+        audioSpeed = Mathf.Log10(pitch.value) * 0.8f;
+        //audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / audioSpeed);
     }
 }
