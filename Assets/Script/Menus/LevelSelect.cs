dd48dc2de5ba6b7590f1754a1f906f73b8b1493a
Simen <simenwiig0@gmail.com>
2022-03-05 15:28:58 +0100

    Music mute and play button

--- Assets/Script/Menus/LevelSelect.cs
+++ Assets/Script/Menus/LevelSelect.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
+    public AudioSource music;
     public void OnClick_Play_Level1()
     {
-        
+        Time.timeScale = 1;
+        music.Play();
     }
     public void OnClick_Back()
     {
--- Assets/Script/Menus/Options.cs
+++ Assets/Script/Menus/Options.cs
@@ -4,6 +4,52 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
+    public AudioSource music;
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+
+        music.mute = muted;
+    }
+    private bool muted = false;
+    public void OnClick_Music()
+    {
+        if(muted == false)
+        {
+            muted = true;
+            music.mute = true;
+        }
+        else
+        {
+            muted = false;
+            music.mute = false;
+        }
+
+        Save();
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+
+    public void OnClick_SFX()
+    {
+
+    }
+
     public void OnClick_Account()
     {
         
@@ -16,4 +62,5 @@ public class Options : MonoBehaviour
     {
         MenuManager.OpenMenu(Menu.Main_Menu, gameObject);
     }
+
 }
