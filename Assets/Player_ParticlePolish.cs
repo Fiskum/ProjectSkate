4a89215a51965295e95e8d244f7836ac8c51616c
Martin Talha <mart97@live.no>
2022-04-20 16:31:07 +0200

    Jumping Animation implemented

--- Assets/Player_ParticlePolish.cs
+++ Assets/Player_ParticlePolish.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player_ParticlePolish : MonoBehaviour
 {
     ParticleSystem[] particles;
-
+    Animator anim;
 
     public float height = 1.38f;
 
@@ -13,6 +13,7 @@ public class Player_ParticlePolish : MonoBehaviour
     void Start()
     {
         particles = GetComponentsInChildren<ParticleSystem>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +25,9 @@ public class Player_ParticlePolish : MonoBehaviour
         Debug.DrawRay(transform.position, Vector2.down * height);
 
         bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, height);
+
+
+        anim.SetBool("isGrounded", isGrounded);
 
         for (int i = 0; i < particles.Length; i++)
             particles[i].enableEmission = isGrounded && horizontalMovement;
--- Assets/Script/Manager_Game.cs
+++ Assets/Script/Manager_Game.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -74,6 +75,7 @@ public class Manager_Game : MonoBehaviour
 
         PauseGame();
         //Unpause();
+
     }
 
 
